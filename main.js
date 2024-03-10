9cd8695530655f94bbdc9be5589c79866569a351
Altabras <Altabras2222@gmail.com>
2024-03-10 14:45:00 +0200

    add js(update)

---
+++ main.js
@@ -0,0 +1,5 @@
+hamburger = document.querySelector('.burger-menu');
+hamburger.onclick = function (){
+    menuBurg = document.querySelector(".menu-burg");
+    menuBurg.classList.toggle("active");
+}
