4eb602c9d4541bff8c549d5f518d7efa4f778db7
Altabras <Altabras2222@gmail.com>
2024-03-10 21:56:28 +0200

    add blur

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
+
 hamburger = document.querySelector('.burger-menu');
-hamburger.onclick = function (){
+contentToBlur = document.querySelector('.content-to-blur');
+footerToBlur = document.querySelector("footer");
+
+hamburger.onclick = function () {
     menuBurg = document.querySelector(".menu-burg");
     menuBurg.classList.toggle("active");
 }
+
+hamburger.onclick = function () {
+    menuBurg = document.querySelector(".menu-burg");
+    menuBurg.classList.toggle("active");
+    hamburger.style.display = 'none';
+    contentToBlur.classList.toggle("blur");
+    footerToBlur.classList.toggle("blur");
+}
+
+
+
+document.addEventListener('click', function (event) {
+
+    if (!menuBurg.contains(event.target) && !hamburger.contains(event.target)) {
+        menuBurg.classList.remove('active');
+        hamburger.style.display = 'block';
+        contentToBlur.classList.remove("blur");
+        footerToBlur.classList.remove("blur");
+    }
+});
+
+
