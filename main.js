bee06d3c2bf66bf52c3c7a62a88c36e0f4e768d8
Altabras <Altabras2222@gmail.com>
2024-03-11 00:36:45 +0200

    update

--- main.js
+++ main.js
@@ -1,31 +1,10 @@
-
-hamburger = document.querySelector('.burger-menu');
-contentToBlur = document.querySelector('.content-to-blur');
-footerToBlur = document.querySelector("footer");
-
-hamburger.onclick = function () {
-    menuBurg = document.querySelector(".menu-burg");
-    menuBurg.classList.toggle("active");
-}
-
-hamburger.onclick = function () {
-    menuBurg = document.querySelector(".menu-burg");
-    menuBurg.classList.toggle("active");
-    hamburger.style.display = 'none';
-    contentToBlur.classList.toggle("blur");
-    footerToBlur.classList.toggle("blur");
-}
-
-
-
-document.addEventListener('click', function (event) {
-
-    if (!menuBurg.contains(event.target) && !hamburger.contains(event.target)) {
-        menuBurg.classList.remove('active');
-        hamburger.style.display = 'block';
-        contentToBlur.classList.remove("blur");
-        footerToBlur.classList.remove("blur");
+let menu = document.querySelector('[data-menu]');
+let burger = document.querySelector('[data-burger]');
+burger.addEventListener('click', ()=>{
+    document.querySelector('body').classList.add('active')
+});
+menu.addEventListener('click', ()=>{
+    if(!event.target.closest('.menu-burg')){
+        document.querySelector('body').classList.remove('active')
     }
 });
-
-
